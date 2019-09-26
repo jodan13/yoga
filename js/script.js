27a7640c3172da1c0ed1fc1efa7be6cca77ce639
Jodan <Jodan@aaanet.ru>
2019-09-26 20:26:23 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
   let form = document.getElementsByClassName("main-form")[0],
     formBottom = document.getElementById("form"),
-    input = document.getElementsByTagName("input"),
+    inputs = document.getElementsByTagName("input"),
     statusMessage = document.createElement("div");
   statusMessage.classList.add("status");
 
@@ -160,4 +160,56 @@ window.addEventListener("DOMContentLoaded", function() {
   }
   sendForm(form);
   sendForm(formBottom);
+
+  // slider
+
+  let slideIndex = 1,
+    slides = document.querySelectorAll(".slider-item"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    dotsWrap = document.querySelector(".slider-dots"),
+    dots = document.querySelectorAll(".dot");
+
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(item => (item.style.display = "none"));
+    // for (let i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = 'none';
+    // }
+    dots.forEach(item => item.classList.remove("dot-active"));
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("dot-active");
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  prev.addEventListener("click", function() {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", function() {
+    plusSlides(1);
+  });
+
+  dotsWrap.addEventListener("click", function(event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (event.target.classList.contains("dot") && event.target == dots[i - 1]) {
+        currentSlide(i);
+      }
+    }
+  });
 });
